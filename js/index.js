c59c6bf9c110d0a23dc62e8796a0d86238cca031
Jhonatan Pacheco da Silva <jhonatan.tec@gmail.com>
2024-02-13 20:26:43 -0300

    atualização de projeto

--- js/index.js
+++ js/index.js
@@ -3,22 +3,43 @@ let pElement = document.querySelector(".retangulo p");
 let imgElement = document.querySelector(".retangulo img");
 let headerElement = document.querySelector(".retangulo h3");
 let buttonElement = document.getElementById("btn-clip");
+let buttonElementDesc = document.getElementById("btn-desc");
+
+let retanguloElement = document.querySelector(".retangulo");
 
 function criptografar() {
   buttonElement.addEventListener("click", () => {
     headerElement.textContent = "Texto Codificado!";
     texto = textareaElement.value;
     resultado = texto
-      .replace(/a/g, "=asd")
-      .replace(/e/g, "@bnm")
-      .replace(/i/g, "+iop")
-      .replace(/o/g, ".opq")
-      .replace(/u/g, "çuyt");
+      .replace(/a/g, "#qzwsx")
+      .replace(/e/g, "dcrfv@")
+      .replace(/i/g, "tgbyh!")
+      .replace(/o/g, "njmkl%")
+      .replace(/u/g, "&pçzxc");
 
     pElement.textContent = resultado;
   });
 }
 
-function remove() {
-  imgElement.parentNode.removeChild(imgElement);
+function descriptografar() {
+  buttonElementDesc.addEventListener("click", () => {
+    texto = textareaElement.value;
+    resultado = texto
+      .replace(/#qzwsx/g, "a")
+      .replace(/dcrfv@/g, "e")
+      .replace(/tgbyh!/g, "i")
+      .replace(/njmkl%/g, "o")
+      .replace(/&pçzxc/g, "u");
+
+    pElement.textContent = resultado;
+  })
 }
+
+function remove() {
+  if (pElement.value) {
+    imgElement.parentNode.removeChild(imgElement);
+  } else {
+
+  }
+} 
